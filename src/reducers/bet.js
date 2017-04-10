b38ccb8048c31196b69f4519aee4223bbf23aafc
melalex <melalex490@gmail.com>
2017-04-10 16:19:49 +0300

    Bet reducer

---
+++ src/reducers/bet.js
@@ -0,0 +1,37 @@
+/**
+ * Created by melalex on 4/10/17.
+ */
+
+import * as actionConst from '../constants/Bet'
+import Progress from "react-progress-2";
+
+const initialState = {
+    odds: 0,
+    balance: 0,
+    fetching: false,
+};
+
+export default function app(state = initialState, action) {
+    switch (action.type) {
+        case actionConst.MAKE_BET_REQUEST:
+        case actionConst.GET_ODS_REQUEST:
+            Progress.show();
+            return {...state, fetching: true};
+
+        case actionConst.MAKE_BET_SUCCESS:
+            Progress.hide();
+            return {...state, fetching: false, balance: action.payload};
+
+        case actionConst.GET_ODS_SUCCESS:
+            Progress.hide();
+            return {...state, fetching: false, odds: action.payload};
+
+        case actionConst.MAKE_BET_FAILED:
+        case actionConst.GET_ODS_FAILED:
+            Progress.hide();
+            return {...state, fetching: false};
+
+        default:
+            return state;
+    }
+};
